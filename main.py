51db09e08ed58bce82ba960434fa3cd4930ffae3
vyastest <142485679+vyastest@users.noreply.github.com>
2023-11-05 12:33:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,15 @@
 import streamlit as st
 import pandas as pd
-from io import BytesIO
-import matplotlib.pyplot as plt
 
-# Sample data
-clicks_data = pd.DataFrame({'Category': ['A', 'B', 'A', 'B', 'C'],
-                            'Clicks': [100, 150, 120, 80, 90]})
-impressions_data = pd.DataFrame({'Campaign': ['X', 'Y', 'X', 'Y', 'Z'],
-                                 'Impressions': [1000, 1200, 800, 900, 700]})
+# Sample DataFrame with 70 rows
+data = pd.DataFrame({'Column1': range(70), 'Column2': range(100, 170)})
 
-# Group clicks by category
-clicks_grouped = clicks_data.groupby('Category').sum()
+# Number of rows to display in each interval
+rows_per_interval = 10
 
-# Create a Streamlit app
-st.title('Clicks and Impressions')
+# Iterate through the DataFrame in intervals
+for i in range(0, len(data), rows_per_interval):
+    st.write(data[i:i + rows_per_interval])
 
-st.subheader('Clicks by Category')
-st.write(clicks_grouped)
-
-st.subheader('Impressions by Campaign')
-st.write(impressions_data)
-
-# Download as PDF
-if st.button("Download as PDF"):
-    # Create a PDF document
-    pdf_buffer = BytesIO()
-    plt.figure(figsize=(8, 6))
-    plt.subplot(2, 1, 1)
-    plt.bar(clicks_grouped.index, clicks_grouped['Clicks'])
-    plt.title('Clicks by Category')
-    
-    plt.subplot(2, 1, 2)
-    plt.bar(impressions_data['Campaign'], impressions_data['Impressions'])
-    plt.title('Impressions by Campaign')
-
-    plt.tight_layout()
-    plt.savefig(pdf_buffer, format='pdf')
-    pdf_buffer.seek(0)
-
-    # Offer the PDF for download
-    import base64
-
-
-    # Assuming you have a PDF file in a bytes variable called pdf_buffer
-    pdf_base64 = base64.b64encode(pdf_buffer).decode('utf-8')
-    link = f'<a href="data:application/pdf;base64,{pdf_base64}" download="report.pdf">Download PDF</a>'
-    st.markdown(link, unsafe_allow_html=True)
-
-
-    st.write('You can click the button to download the report as a PDF.')
+    # Add a separator to distinguish between intervals
+    st.write('---')
