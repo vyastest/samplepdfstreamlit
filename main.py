7b71033326f2ad2b00a5fe98fd8af1b3ab65b48c
vyastest <142485679+vyastest@users.noreply.github.com>
2023-11-05 12:45:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ data = pd.DataFrame({'Column1': range(70), 'Column2': range(100, 170)})
 # Number of rows to display in each interval
 rows_per_interval = 10
 
+st.header("world cup games")
 # Iterate through the DataFrame in intervals
 for i in range(0, len(data), rows_per_interval):
     st.write(data[i:i + rows_per_interval])
